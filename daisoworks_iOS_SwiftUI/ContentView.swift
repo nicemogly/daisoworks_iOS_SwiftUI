11a6f02150470d533741a6207eb4d131ec09839d
nicemogly <newmogly@naver.com>
2024-10-07 13:29:29 -0700

    Initial Commit

---
+++ daisoworks_iOS_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  daisoworks_iOS_SwiftUI
+//
+//  Created by AD2201016P02 on 10/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
